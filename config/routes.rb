ee8d1bc4dcfc7b84a381ea02aac3577cf14f1e28
Jami Owen <jamirene88@gmail.com>
2018-11-28 10:52:50 +0000

    created a app for generating lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # R - read
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # C - create
+
+  post 'tasks', to: 'tasks#create'
+
+
+  patch 'tasks/:id', to: 'tasks#update'
+# patch 'restaurants/:id', to: 'restaurants#update'
+
+  delete "tasks/:id", to: "tasks#destroy"
 end
